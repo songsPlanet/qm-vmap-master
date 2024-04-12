cae02e2f1b5745b734dfe8a4ee9f16b2bb21aca9
chenzhaodihuya@163.com <chenzhaodihuya@16.com>
2024-04-12 11:00:26 +0800

    feat: 固定弹窗

--- src/gis/widget/PopupWrapper/index.vue
+++ src/gis/widget/PopupWrapper/index.vue
@@ -4,15 +4,6 @@ import type { TPouperData } from '../PopupPanel/index.vue'
 import { Popup } from 'mapbox-gl'
 import Compone from './index'
 
-// let defaultXY = {
-//   x: 0,
-//   y: 0
-// }
-// let divOffset = {
-//   l: 0,
-//   t: 0
-// }
-
 type PopupEvent = {
   type: 'open' | 'close'
   target: Popup
@@ -28,7 +19,6 @@ const popup = ref()
 const containerRef = ref()
 const map = inject<any>('map')
 const props = defineProps<TPopupWrapper>()
-
 const emit = defineEmits<{
   closeHandle: [value: PopupEvent]
   openHandle: [value: PopupEvent]
@@ -43,69 +33,34 @@ const popupComp = computed<Popup>(() => {
   return pp
 })
 popup.value = popupComp.value
-// const ppHeader = document.getElementsByClassName('popup-header-wrap')[0]
-// const ppContainer = ref((popup.value as any)._container as HTMLElement)
 
 const onCloseHandle = (e: any) => {
   emit('closeHandle', e as PopupEvent)
 }
 
-// const mousedown = (e: any) => {
-//   e.preventDefault() // 阻止事件默认行为
-//   defaultXY = {
-//     x: e.clientX,
-//     y: e.clientY
-//   }
-//   divOffset = {
-//     l: ppContainer.value.offsetLeft,
-//     t: ppContainer.value.offsetTop
-//   }
-//   ppHeader.addEventListener('mousemove', mousemove)
-// }
-
-// const mousemove = (e: any) => {
-//   if (ppContainer) {
-//     // 获取x和y
-//     let nx = e.clientX
-//     let ny = e.clientY
-//     let nl = nx - (defaultXY.x - divOffset.l)
-//     let nt = ny - (defaultXY.y - divOffset.t)
-//     ppContainer.value.style.left = nl + 'px'
-//     ppContainer.value.style.top = nt + 'px'
-//   }
-// }
-
-// const mouseup = () => {
-//   ppHeader.removeEventListener('mousemove', mousemove)
-// }
+const updataPop = () => {
+  if (popup.value.isOpen()) {
+    popup.value.setLngLat(props.popupData.lngLat)
+  }
+  const titleElem = document.getElementById('popup-title-wrap-id')
+  titleElem!.innerText = props.popupData.title
+}
 
 onMounted(() => {
   popup.value.on('close', onCloseHandle)
+  updataPop()
   map.value && popup.value.setDOMContent(containerRef.value).addTo(map.value)
-  if (props.enableDrag) {
-    // ppHeader.addEventListener('mousedown', mousedown)
-    // ppHeader.addEventListener('mouseup', mouseup)
-  }
 })
 
 watch(
   () => props.popupData,
   () => {
-    popup.value.on('close', onCloseHandle)
-    if (popup.value.isOpen()) {
-      popup.value.setLngLat(props.popupData.lngLat)
-    }
-
-    const titleElem = document.getElementById('popup-title-wrap-id')
-    titleElem!.innerText = props.popupData.title
-  },
-  { deep: true }
+    updataPop()
+  }
 )
 
 onUnmounted(() => {
   popup.value.off('close', onCloseHandle)
-  // ppHeader?.removeEventListener('mousedown', mousedown)
-  // ppHeader?.removeEventListener('mouseup', mouseup)
   if (popup.value.isOpen()) {
     popup.value.remove()
   }
